642fad696b879ea79a79f878d4d502756cd02d3c
Adler Murga <adlermurga@gmail.com>
2026-05-11 19:03:29 +0000

    medicamentos/urls modificados

--- medicamento/urls.py
+++ medicamento/urls.py
@@ -6,10 +6,10 @@ app_name = "medicamento"
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("", views.listar, name="listar"),           
-    path("cadastrar/", views.cadastrar, name="cadastrar"), 
-    path("<int:pk>/", views.detalhe, name="detalhe"), 
-    path("<int:pk>/remover/", views.remover, name="remover"),
+    path("medicamentos/", views.listar, name="listar"),
+    path("medicamentos/cadastrar/", views.cadastrar, name="cadastrar"),
+    path("medicamentos/<int:pk>/", views.detalhe, name="detalhe"),
+    path("medicamentos/<int:pk>/remover/", views.remover, name="remover"),
     path("alertas/", views.alertas, name="alertas"),
     path("alertas/<int:pk>/confirmar/", views.confirmar_uso, name="confirmar_uso"),
     path("bula/<str:nome_medicamento>/", views.buscar_bula_medicamento, name="buscar_bula"),
