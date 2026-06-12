fee48a67ccb557debd991afe801c3f3a4a82932d
Vinicius665a <viniciuslopesm665@gmail.com>
2026-06-11 22:47:47 -0300

    feat: implementa a funcionalidade de atualizar medicamento

--- medicamento/views.py
+++ medicamento/views.py
@@ -9,7 +9,7 @@ from django.views.decorators.http import require_GET
 from requests.exceptions import Timeout
 
 from .models import HorarioMedicamento, Medicamento, RegistroUso
-from .traducao import traduzir_texto as traduzir_texto_para_portugues
+
 
 
 def index(request):
